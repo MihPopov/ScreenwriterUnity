e0d57cfeb5eee2275eff4adff65ea05f58ca5935
Arslan <galeev_arslan2021@bk.ru>
2025-07-03 08:43:00 +0300

    "Street" map was made, player control and workpiece for json file loading were added

---
+++ Assets/Scripts/CharacterClasses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class CharacterClass
+{
+    public string Id;
+    public string Name;
+    public List<DataClass> Data;
+}
+
+[Serializable]
+public class DataClass
+{
+    public string Id;
+    public string Line;
+    public ToClass To;
+}
+
+[Serializable]
+public class ToClass
+{
+    public string Id;
+    public string Info;
+    public string Line;
+}
